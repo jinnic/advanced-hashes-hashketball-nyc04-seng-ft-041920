3dbdf202758401d6bc329f1fc2c127949403db51
Hyojin Yoo <yoohyojin@gmail.com>
2020-05-05 16:30:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -246,4 +246,17 @@ end
 
 def big_shoe_rebounds
   
+  big_shoe = 0
+  
+  #loop key(:home, :away) and get value(hash)
+  game_hash.each do |key, value|
+    #loop value[:players](array) player(hash)
+    value[:players].each do |player| 
+      if big_shoe < player[:shoe] 
+        big_shoe = player[:shoe] 
+      end
+    end
+    
+  end
+      
 end
